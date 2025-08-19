059bebb4347cf63fd68c9ae8067ae414e92cf3d5
0zer0knight <yukinotacheese@proton.me>
2025-08-19 03:00:28 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
+#this is for daily cleaner system
 import os
-import schedule
 from datetime import datetime
-import time
-
-def cleaner():
-        ctime = datetime.now()
-        gettime = ctime.strftime("%H:%M:%S")
-
-        os.system('sudo dnf clean all ; sudo dnf autoremove ; sudo dnf remove $(dnf repoquery --installonly --latest-limit=-2 -q) ; sudo journalctl --vacuum-time=2weeks ; sudo find /tmp -type f -atime +10 -delete ; rm -rf ~/.cache/* ; sudo dnf makecache ; ')
-
-        with open('log.txt', 'a') as file:
-            file.write(f"pembersihan dilakukan pada pukul {gettime}\n")
-
 
 def main():
-        userask = int(input("pembersihan mau diulang berapa jam = "))
-        try:
-            print(f'program cleaner akan diulang {userask} jam sekali')
+    waktu = datetime.now()
+    user = os.getlogin()
+    os.system("sudo apt-get remove ; sudo apt-get clean ; sudo apt-get purge ; sudo journalctl --vacuum-time=2weeks ; sudo find /tmp -type f -atime +10 -delete ; rm -rf ~/.cache/* ; sudo apt-get makecache ;")
 
-            cleaner()
-            schedule.every(userask).hours.do(cleaner)
+    with open("datalog.txt", "a") as file:
+        file.write(f"user : {user} telah melakukan pembersihan pada waktu = {waktu} \n")
 
-            while True:
-                 schedule.run_pending()
-                 time.sleep(1)
-                
-        except ValueError as e:
-            print("bukan input yang valid")
 
-        
 if __name__ == "__main__":
-    main()
+       main()
